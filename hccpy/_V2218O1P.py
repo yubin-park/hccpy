17fce93b2080a70619b84cb58f81027aabb17bd7
David Roberts <robertsdav@dvaco.org>
2021-09-27 20:26:06 -0400

    Updated V24 interaction codes to match V24hcccoefn.csv. Updated get_risk_dct() to include medicaid parameter to address missing institutional, medicaid interaction. Used .get() to be a bit more concise in defaulting risk scores.

--- hccpy/_V2218O1P.py
+++ hccpy/_V2218O1P.py
@@ -1,15 +1,15 @@
 
-def get_risk_dct(coefn, hcc_lst, age, sex, elig, origds):
+def get_risk_dct(coefn, hcc_lst, age, sex, elig, origds, medicaid):
 
     risk_dct = {}
 
-    # demographic factors
-
+    # build demographic bracket strings and add to risk_dct
     elig_demo = elig 
     if elig[:3] in {"CFA", "CFD", "CNA", "CND", "CPA", "CPD", "INS"}:
         elig_demo += "_" 
     elig_demo += sex
 
+    # build age bracket strings and add to risk_dct
     age_ranges = [x for x in coefn.keys() if elig_demo in x]
     age_match = ""
     for age_range in age_ranges:
@@ -26,29 +26,26 @@ def get_risk_dct(coefn, hcc_lst, age, sex, elig, origds):
         if lb <= age < ub:
             age_match = age_range
             break
-    if age_match in coefn:
-        risk_dct[age_match] = coefn[age_match]
-    elif age_match != "":
-        risk_dct[age_match] = 0.0 # NOTE: default value
-
+    risk_dct[age_match] = coefn.get(age_match, 0.0)
+    
+    # build original entitlement string and add to risk_dct
     if origds > 0:
         elig_origds = elig + "_OriginallyDisabled_"
         if sex == "M":
             elig_origds += "Male" 
         else:
-            elig_origds += "Female" 
-        if elig_origds in coefn:
-            risk_dct[elig_origds] = coefn[elig_origds]
-        else:
-            risk_dct[elig_origds] = 0.0 # NOTE: default value
+            elig_origds += "Female"     
+        risk_dct[elig_origds] = coefn.get(elig_origds, 0.0)
+    
+    # build medicaid interacion and add to risk_dict
+    if medicaid:
+        mcd_hcc = elig + "_" + "LTIMCAID"
+        risk_dct[mcd_hcc] = coefn.get(mcd_hcc, 0.0)
 
-    # hcc factors
+    # build hcc factor strings and add to risk_dict
     for hcc in hcc_lst:
         elig_hcc = elig + "_" + hcc
-        if elig_hcc in coefn:
-            risk_dct[elig_hcc] = coefn[elig_hcc]
-        else:
-            risk_dct[elig_hcc] = 0.0 # NOTE: default value
+        risk_dct[elig_hcc] = coefn.get(elig_hcc, 0.0)
 
     return risk_dct
 
